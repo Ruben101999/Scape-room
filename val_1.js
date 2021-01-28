1f33c1208eca98046cc4efba9b03ce252639a0ec
Ruben <rubenortegagu@gmail.com>
2021-01-28 12:21:55 +0100

    28/01/2021

---
+++ val_1.js
@@ -0,0 +1,7 @@
+function val_1() {
+    var a = document.getElementById('val_1').value
+    var a = document.getElementById('val_2').value
+    if (a=='A' && b=='starly'){
+        return true
+    }
+}
