e337fcb91b11fae5fb15dea5da2c178060cc9384
Suarezjoan <71765713+Suarezjoan@users.noreply.github.com>
2021-01-31 19:43:50 +0100

    subida jm

---
+++ js/codigojoan.js
@@ -0,0 +1,19 @@
+function val_1() {
+    var a = document.getElementById('val-1').value
+    if (a == 'Peru') {
+        return true
+    } else {
+        alert('¡Intentalo de nuevo Crack!')
+    }
+    return false
+}
+
+function val_2(){
+    var a = document.getElementById('val-2').value
+    if (a == 'Bolivia') {
+        return true
+    } else {
+        alert('¡Intentalo de nuevo Crack!')
+    }
+    return false
+}
